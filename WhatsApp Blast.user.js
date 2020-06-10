e94b475bd695f90591e589fc30ad37653a36fc0c
Rizal Nurhidayat <43670693+rzlnhd@users.noreply.github.com>
2020-06-10 08:44:53 +0700

    Patch 3.5.1
    ▫ Fixing duplicate panel.
    ▫ Fixing back function.

--- WhatsApp Blast.user.js
+++ WhatsApp Blast.user.js
@@ -6,9 +6,11 @@
 // @icon         https://raw.githubusercontent.com/rzlnhd/WhatsApp-Blast/master/assets/icon.png
 // @homepageURL  https://github.com/rzlnhd/WhatsApp-Blast
 // @supportURL   https://github.com/rzlnhd/WhatsApp-Blast/issues
-// @version      3.5.0
-// @date         2020-6-6
+// @version      3.5.1
+// @date         2020-6-10
 // @author       Rizal Nurhidayat
+// @include      http://*
+// @include      https://*
 // @match        https://web.whatsapp.com/
 // @grant        GM_getResourceText
 // @grant        GM_xmlhttpRequest
@@ -142,7 +144,7 @@ class Interval {
     Initial Function
 =====================================*/
 /** Global Variables */
-const version = "v3.5.0", upDate = "6 Juni 2020", qACR = "._13opk",
+const version = "v3.5.1", upDate = "10 Juni 2020", qACR = "._13opk",
     qInp = "#main div[contenteditable='true']", qSend = "#main span[data-icon='send']",
     queue = new Queue(), mesej = new Message(), doBlast = new Interval(), report = new Report(),
     xmlReq = ("function" == typeof GM_xmlhttpRequest) ? GM_xmlhttpRequest : GM.xmlhttpRequest,
@@ -161,9 +163,12 @@ console.log("WhatsApp Blast " + version + " - Waiting for WhatsApp to load...");
 var timer = setInterval(general, 1000);
 function general(){
     if (getElm("div.two")){
-        let pnl = getById("side"), itm = getElm("header"), e = itm.cloneNode(true);
-        loadModule(); initComponents(e); pnl.insertBefore(e, pnl.childNodes[1]); initListener();
-        console.log("WhatsApp Blast " + version + " - Blast Your Follow Up NOW!"); clearInterval(timer);
+        let head = getElmAll("header"); if(head.length<2){
+            let pnl = getById("side"), itm = getElm("header"), e = itm.cloneNode(true);
+            loadModule(); initComponents(e); pnl.insertBefore(e, pnl.childNodes[1]); initListener();
+            console.log("WhatsApp Blast " + version + " - Blast Your Follow Up NOW!");
+        }
+        getingData(); clearInterval(timer);
     }
 }
 /** Load WAPI Module for Send Message & Image */
@@ -215,10 +220,10 @@ function initListener(){
     clk.forEach(e => {getById(e.id).addEventListener("click", e.fn);});
     tab.forEach(e => {e.addEventListener("click", openMenu);});
     trg.forEach(e => {e.addEventListener("click", checking);});
-    wbH.addEventListener("click", toggleApp); getingData();
     getById("getFile").addEventListener("change", prevDat);
     getById("getImg").addEventListener("change", prevImg);
     getById("message").addEventListener("input", superBC);
+    wbH.addEventListener("click", toggleApp);
     tab[0].click(); if(opn)wbH.click();
 }
 /**=====================================
@@ -344,11 +349,11 @@ function setUser(u){
 =====================================*/
 /** Setting "BLAST" Status */
 function setStatus(stat){
-    let path = getById("blast"), ico = getById("blastIc"),
+    let path = getById("blast"), ico = getById("blastIc"), side = getById("pane-side"),
         stopIc = "M505.16405,19.29688c-1.176-5.4629-6.98736-11.26563-12.45106-12.4336C460.61647,0,435.46433,0,410.41962,0,307.2013,0,245.30155,55.20312,199.09162,128H94.88878c-16.29733,0-35.599,11.92383-42.88913,26.49805L2.57831,253.29688A28.39645,28.39645,0,0,0,.06231,264a24.008,24.008,0,0,0,24.00353,24H128.01866a96.00682,96.00682,0,0,1,96.01414,96V488a24.008,24.008,0,0,0,24.00353,24,28.54751,28.54751,0,0,0,10.7047-2.51562l98.747-49.40626c14.56074-7.28515,26.4746-26.56445,26.4746-42.84374V312.79688c72.58882-46.3125,128.01886-108.40626,128.01886-211.09376C512.07522,76.55273,512.07522,51.40234,505.16405,19.29688ZM384.05637,168a40,40,0,1,1,40.00589-40A40.02,40.02,0,0,1,384.05637,168ZM35.68474,352.06641C9.82742,377.91992-2.94985,442.59375.57606,511.41016c69.11565,3.55859,133.61147-9.35157,159.36527-35.10547,40.28913-40.2793,42.8774-93.98633,6.31147-130.54883C129.68687,309.19727,75.97,311.78516,35.68474,352.06641Zm81.63312,84.03125c-8.58525,8.584-30.08256,12.88672-53.11915,11.69922-1.174-22.93555,3.08444-44.49219,11.70289-53.10938,13.42776-13.42578,31.33079-14.28906,43.51813-2.10352C131.60707,404.77148,130.74562,422.67188,117.31786,436.09766Z",
         blastIc = "M505.12019,19.09375c-1.18945-5.53125-6.65819-11-12.207-12.1875C460.716,0,435.507,0,410.40747,0,307.17523,0,245.26909,55.20312,199.05238,128H94.83772c-16.34763.01562-35.55658,11.875-42.88664,26.48438L2.51562,253.29688A28.4,28.4,0,0,0,0,264a24.00867,24.00867,0,0,0,24.00582,24H127.81618l-22.47457,22.46875c-11.36521,11.36133-12.99607,32.25781,0,45.25L156.24582,406.625c11.15623,11.1875,32.15619,13.15625,45.27726,0l22.47457-22.46875V488a24.00867,24.00867,0,0,0,24.00581,24,28.55934,28.55934,0,0,0,10.707-2.51562l98.72834-49.39063c14.62888-7.29687,26.50776-26.5,26.50776-42.85937V312.79688c72.59753-46.3125,128.03493-108.40626,128.03493-211.09376C512.07526,76.5,512.07526,51.29688,505.12019,19.09375ZM384.04033,168A40,40,0,1,1,424.05,128,40.02322,40.02322,0,0,1,384.04033,168Z";
-    console.log(stat ? (ico.setAttribute("d", stopIc), path.setAttribute("title", "STOP!"), "Blasting...")
-        : (ico.setAttribute("d", blastIc), path.setAttribute("title", "BLAST!"), "Stoped."));
+    side.style.overflowY = (stat ? (ico.setAttribute("d", stopIc), path.setAttribute("title", "STOP!"), console.log("Blasting..."), 'hidden')
+        : (ico.setAttribute("d", blastIc), path.setAttribute("title", "BLAST!"), console.log("Stoped."), 'auto'));
     doing = stat;
 }
 /** Update UI */
@@ -478,7 +483,10 @@ function openMenu(e){
 /** Show Change Log */
 function changeLog(){
     let cLog = "WhatsApp Blast " + version + " (Last Update: " + upDate + ").";
-    cLog += "\n▫ Memperbaiki Error akibat Update WhatsApp."
+    cLog += "\n▫ Memperbaiki error duplikasi panel."
+        + "\n▫ Memperbaiki fungsi kembali ke chatroom."
+        + "\n\nVersion v3.5.0 (5 Juni 2020)."
+        + "\n▫ Memperbaiki Error akibat Update WhatsApp."
         + "\n▫ Memperbarui Logic & Engine."
         + "\n▫ Menerapkan konsep Queue dan OOP."
         + "\n▫ Mengganti Server Data Pengguna Premium."
@@ -489,11 +497,7 @@ function changeLog(){
         + "\n▫ Reset Masa Trial untuk Pengguna Premium."
         + "\n\nVersion v3.4.14 (11 Mei 2020)."
         + "\n▫ Memperbaiki fitur pengiriman gambar."
-        + "\n▫ Perbaikan Minor."
-        + "\n\nVersion v3.4.13 (9 Apr 2020)."
-        + "\n▫ Menambah fitur Trial 7 Hari."
-        + "\n▫ Memperbaiki bug major."
-        + "\n▫ Refactoring Code.";
+        + "\n▫ Perbaikan Minor.";
     alert(cLog);
 }
 /**=====================================
